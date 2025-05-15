f07bcb6bc32fe7e9733af08aef3e951ac7f233a4
Magdalena Kubincova <magdalena.kubincova@maibornwolff.de>
2025-05-15 16:33:16 +0200

    feat: add skip navigation link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -5,11 +5,16 @@
 </script>
 
 <div class="h-screen bg-secondary text-primary p-4">
+	<a
+		href="#main"
+		class="fixed top-full left-0 right-0 text-center bg-accent p-2 z-100 focus:top-0 text-link"
+		>Skip to main content</a
+	>
 	<Sidebar>
 		<Navigation />
 		<Footer />
 	</Sidebar>
-	<main class="overflow-auto h-full lg:ml-[calc(var(--sidebar-width))] pl-4">
+	<main id="main" class="overflow-auto h-full lg:ml-[calc(var(--sidebar-width))] pl-4">
 		{@render children()}
 	</main>
 </div>
